ed2af97a399b7973d7ec48fa7cdbfb7ec34074f7
jiro_chan03 <hayashin0919@gmail.com>
2024-09-04 17:22:19 +0900

    index-css-up-load

--- js/app-onbarding.js
+++ js/app-onbarding.js
@@ -11,3 +11,39 @@ closeBtn.addEventListener('click',function(){
     const model=document.querySelector('.model-wrap');
     model.style.display="none";
 });
+
+{
+    const slider=document.querySelector("#slider");
+    const items=document.querySelectorAll("li");
+    const nextBtn=document.querySelector("#next");
+    const prevBtn=document.querySelector("#prev");
+
+    let index=0;
+
+    nextBtn.onclick=function(event){
+        let next;
+        if(index<items.length-1){
+            next=index+1;
+        }
+        else{
+            next=0;
+        }
+        items[index].style.display="none";
+        items[next].style.display="block"
+        index=next;
+    }
+
+    prevBtn.onclick=function(event){
+        let prev;
+        if(index>=items.length-14){
+            prev=index-1;
+        }
+        else{
+            prev=14;
+        }
+        items[index].style.display="none";
+        items[prev].style.display="block"
+        index=prev;
+    }
+
+}
